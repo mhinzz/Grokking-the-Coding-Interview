33f0f1f6023114bdfabed52ebe93570c11f3da53
Matthew Hinz <mhinz@shaw.ca>
2022-12-16 16:39:54 -0500

    Update LinkedList Cycle.cs

---
+++ 03 Fast & Slow Pointers/01 LinkedList Cycle/LinkedList Cycle.cs
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public bool HasCycle(ListNode head) {
+        if (head == null)
+            return false;
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (slow == fast)
+                return true;
+        }
+        return false;
+    }
+}
